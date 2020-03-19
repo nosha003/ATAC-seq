c33539a5302547790422a00c47bea148d71034ee
nosha003 <32301282+nosha003@users.noreply.github.com>
2020-03-19 12:30:36 -0500

    Create 09_cpm.sh

---
+++ 09_cpm.sh
@@ -0,0 +1,17 @@
+#13Jan2020
+#cpm
+
+#!/bin/bash
+#PBS -l walltime=08:00:00,nodes=1:ppn=6,mem=40gb
+#PBS -o /scratch.global/nosha003/e_o/cpm_o
+#PBS -e /scratch.global/nosha003/e_o/cpm_e
+#PBS -N cpm
+#PBS -V
+#PBS -r n
+
+cd /scratch.global/nosha003/atac/
+module load R/3.5.0
+R CMD BATCH all.2.uniq.cpm.R
+wait
+
+# qsub /scratch.global/nosha003/atac/cpm.sh
