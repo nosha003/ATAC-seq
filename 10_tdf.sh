6f69cb73c9149f75800c12e414d10566ea2c7e26
nosha003 <32301282+nosha003@users.noreply.github.com>
2020-03-19 12:35:15 -0500

    Update 10_tdf.sh

--- 10_tdf.sh
+++ 10_tdf.sh
@@ -23,3 +23,6 @@ cd /scratch.global/nosha003/atac/align
 /home/springer/nosha003/igv/IGVTools/igvtools toTDF ${ID}.igv ${ID}.tdf /home/springer/nosha003/database/B73v4/Zea_mays.AGPv4.dna.genome.fa
 
 #qsub -t 1-4 -v LIST=/home/springer/nosha003/schmitz/atac_merge.txt /scratch.global/nosha003/atac/tdf.sh
+
+#scp nosha003@login.msi.umn.edu:/scratch.global/nosha003/atac/align/*.tdf /Volumes/CBS/Groups/LAB-springer/Jaclyn/Graduate_projects/igv/.
+
